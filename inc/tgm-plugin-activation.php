c3dba40e86ab14742b7838959391ebdd827aacec
Bruno Luiz <contact@brunoluiz.net>
2017-01-08 14:48:12 -0200

    Added custom post type

--- inc/tgm-plugin-activation.php
+++ inc/tgm-plugin-activation.php
@@ -73,6 +73,19 @@ function ads_register_required_plugins() {
       'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
     ),
 
+    array(
+      'name'               => 'ADS Portfolio Custom Post and Taxonomy Types', // The plugin name.
+      'slug'               => 'ads-portfolio', // The plugin slug (typically the folder name).
+      'source'             => get_template_directory() . '/lib/plugins/ads-portfolio.zip', // The plugin source.
+      // 'source'             => 'https://github.com/wp-premium/advanced-custom-fields-pro/archive/master.zip',
+      'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+      'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+      'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+      'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+      'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+      'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+    ),
+
     // // This is an example of how to include a plugin from an arbitrary external source in your theme.
     // array(
     //   'name'         => 'TGM New Media Plugin', // The plugin name.
---
+++ lib/plugins/ads-portfolio/ads-portfolio.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Plugin Name: ADS Portfolio Custom Post and Taxonomy Types
+ * Plugin URI: #
+ * Version: 1.0
+ * Author: Bruno Luiz
+ * Author URI: http://brunoluiz.net
+ * Description: A simple portfolio system
+ */
+
+add_action('init', 'add_custom_post_type');
+add_action('init', 'add_custom_taxonomies');
+
+function add_custom_post_type() {
+  register_post_type( 'project', array(
+    'labels' => array(
+      'name'               => _x( 'Portfolio projects', 'post type general name', 'ads-project' ),
+      'singular_name'      => _x( 'Project', 'post type singular name', 'ads-project' ),
+      'menu_name'          => _x( 'Projects', 'admin menu', 'ads-project' ),
+      'name_admin_bar'     => _x( 'Portfolio Project', 'add new on admin bar', 'ads-project' ),
+      'add_new'            => _x( 'Add New Project', 'project', 'ads-project' ),
+      'add_new_item'       => __( 'Add New Project', 'ads-project' ),
+      'new_item'           => __( 'New Project', 'ads-project' ),
+      'edit_item'          => __( 'Edit Project', 'ads-project' ),
+      'view_item'          => __( 'View Project', 'ads-project' ),
+      'all_items'          => __( 'All Projects', 'ads-project' ),
+      'search_items'       => __( 'Search Projects', 'ads-project' ),
+      'parent_item_colon'  => __( 'Parent Projects:', 'ads-project' ),
+      'not_found'          => __( 'No project found.', 'ads-project' ),
+      'not_found_in_trash' => __( 'No project found in Trash.', 'ads-project' ),
+    ),
+    // Frontend
+    'has_archive'        => true,
+    'public'             => true,
+    'publicly_queryable' => true,
+    // Admin
+    'capability_type'    => 'post',
+    'menu_icon'          => 'dashicons-businessman',
+    'menu_position'      => 20,
+    'query_var'          => true,
+    'show_in_menu'       => true,
+    'show_ui'            => true,
+    'supports'           => array('title', 'editor', 'thumbnail', 'revisions'),
+    'taxonomies' => array('skill')
+  ));
+
+  wp_insert_term(
+    'Portfolio Categories', // the term
+    'category',             // the taxonomy
+    array(
+      'description'=> 'Portfolio Categories',
+      'slug' => 'portfolio'
+    ));
+}
+
+function add_custom_taxonomies() {
+  register_taxonomy('skill', 'project', array(
+    // Hierarchical taxonomy (like categories)
+    'hierarchical' => true,
+    'public' => true,
+    // This array of options controls the labels displayed in the WordPress Admin UI
+    'labels' => array(
+      'name' => _x( 'Skills', 'taxonomy general name' ),
+      'singular_name' => _x( 'Skill', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Skills' ),
+      'all_items' => __( 'All Skills' ),
+      'parent_item' => __( 'Parent Skill' ),
+      'parent_item_colon' => __( 'Parent Skill:' ),
+      'edit_item' => __( 'Edit Skill' ),
+      'update_item' => __( 'Update Skill' ),
+      'add_new_item' => __( 'Add New Skill' ),
+      'new_item_name' => __( 'New Skill Name' ),
+      'menu_name' => __( 'Skills' ),
+    ),
+    // Control the slugs used for this taxonomy
+    'rewrite' => array(
+      'slug' => 'skills', // This controls the base slug that will display before each term
+      'with_front' => false, // Don't display the category base before "/locations/"
+      'hierarchical' => true, // This will allow URL's like "/locations/boston/cambridge/"
+    )
+  ));
+}
