933a7db319ed4d4fb192517cc4d69b7fa01478dc
Bruno Luiz <contact@brunoluiz.net>
2017-01-09 04:56:06 -0200

    Started Page layout

--- assets/views/content-page.php
+++ assets/views/content-page.php
@@ -10,9 +10,10 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<div class='banner'>
+    <div class='banner-background'></div>
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+  </div>
 
 	<div class="entry-content">
 		<?php
