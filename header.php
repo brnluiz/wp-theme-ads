2fd17fcf799dbd1ced0545390d74dcb2169ff367
Bruno Luiz <contact@brunoluiz.net>
2017-01-16 23:57:53 -0200

    Added a favicon

--- header.php
+++ header.php
@@ -15,6 +15,8 @@
 <meta charset='<?php bloginfo( 'charset' ); ?>'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link rel='profile' href='http://gmpg.org/xfn/11'>
+<link rel='shortcut icon' href='<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico' />
+<meta name='description' content='Andre Diogo da Silva Portfolio' />
 
 <?php wp_head(); ?>
 </head>
